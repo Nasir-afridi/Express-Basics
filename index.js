762709cf039f627d75f5b749e203f7ca15f35abd
Nasir-afridi <nasirwc05@gmail.com>
2025-01-27 13:57:43 +0500

    Added the error handling function with the error route

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const app = express();
 const port  = 4000;
 
 
-//Middleware
+//*******************Middleware
 app.use((req, res, next) => {
   console.log('middleware 1');
   next(); // agrr ye na chalaya to control next route prr nahe jayga.
 });
 
-// configure ejs
+// *****************configure ejs
 app.set("view engine", "ejs");
+
+// ********************path for the static files.
+app.use(express.static('public'));
 
 //Routes
 app.get('/home', (req, res) => {
@@ -21,7 +24,7 @@ app.get('/profile/:username', (req, res) => { // :username is a parameter we can
   res.send(`hello this is me ${req.params.username}`); // if we want to print the same name of the url in the body we use this.
 });
 
-// ejs api
+// **********************ejs api
 app.get('/', (req, res) => {
   res.render("index", {age:12});  // we also pass data from it 
 });
@@ -30,8 +33,18 @@ app.get('/contact', (req, res) => {
   res.render("contact", {name:"Muhammad Nasir"});
 });
 
-// path for the static files.
-app.use(express.static('public'));
+app.get('/error', (req, res, next) => {
+throw Error("something went wrong")}
+);
+
+//***************** */ Error handling
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+});
 
 app.listen(4000, () => {
   console.log(`Server is running on port : ${port}`);
